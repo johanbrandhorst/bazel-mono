eb82acee80d7f3d74af51b3c9da778a8720d1c22
Johan Brandhorst <johan.brandhorst@gmail.com>
2020-05-30 17:55:35 +0100

    service/go-server/users: fix copying of mutex locks

--- service/go-server/users/types.go
+++ service/go-server/users/types.go
@@ -48,8 +48,8 @@ func (rw *roleWrapper) Scan(in interface{}) error {
 type timeWrapper timestamp.Timestamp
 
 // Value implements database/sql/driver.Valuer for timestamp.Timestamp
-func (tw timeWrapper) Value() (driver.Value, error) {
-	return ptypes.Timestamp((*timestamp.Timestamp)(&tw))
+func (tw *timeWrapper) Value() (driver.Value, error) {
+	return ptypes.Timestamp((*timestamp.Timestamp)(tw))
 }
 
 // Scan implements database/sql/driver.Scanner for timestamp.Timestamp
@@ -60,20 +60,19 @@ func (tw *timeWrapper) Scan(in interface{}) error {
 		return err
 	}
 
-	tp, err := ptypes.TimestampProto(t.Time)
-	if err != nil {
-		return err
-	}
+	*tw = timeWrapper(timestamp.Timestamp{
+		Seconds: t.Time.Unix(),
+		Nanos:   int32(t.Time.Nanosecond()),
+	})
 
-	*tw = (timeWrapper)(*tp)
 	return nil
 }
 
 type durationWrapper duration.Duration
 
 // Value implements database/sql/driver.Valuer for duration.Duration
-func (dw durationWrapper) Value() (driver.Value, error) {
-	d, err := ptypes.Duration((*duration.Duration)(&dw))
+func (dw *durationWrapper) Value() (driver.Value, error) {
+	d, err := ptypes.Duration((*duration.Duration)(dw))
 	if err != nil {
 		return nil, err
 	}
